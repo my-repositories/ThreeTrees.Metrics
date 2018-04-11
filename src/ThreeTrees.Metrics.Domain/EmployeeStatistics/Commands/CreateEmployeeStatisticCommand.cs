ce6303fe9305278dc282be7e976ec461d3c2fc68
loktionov129 <loktionov129@gmail.com>
2018-04-11 18:01:01 +0700

    Create Domain commands, handlers and queries

---
+++ src/ThreeTrees.Metrics.Domain/EmployeeStatistics/Commands/CreateEmployeeStatisticCommand.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) ThreeTrees. All rights reserved.
+// Licensed under the BSD license. See LICENSE file in the project root for full license information.
+
+namespace ThreeTrees.Metrics.Domain.EmployeeStatistics.Commands
+{
+    /// <summary>
+    /// Create employee statistic command.
+    /// </summary>
+    public class CreateEmployeeStatisticCommand
+    {
+    }
+}
